68a7e2ec335357ad4185d2098f874aee82eab74d
Lulu-Soso <lucas.sokha@gmail.com>
2023-07-06 14:23:07 +0200

    create reducers user and post

---
+++ src/reducers/post.reducer.js
@@ -0,0 +1,7 @@
+// State manager pour gérer les états
+const initialState = {}
+
+export default function postReducer(state = initialState, action) {
+  // switch pour recevoir tel type d'action et l'exécuter
+  return state;
+}
---
+++ src/reducers/user.reducer.js
@@ -0,0 +1,7 @@
+// State manager pour gérer les états
+const initialState = {}
+
+export default function userReducer(state = initialState, action) {
+  // switch pour recevoir tel type d'action et l'exécuter
+  return state;
+}
